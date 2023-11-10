22602d8a3c31628fa5e604b7dcda5d4757209b7c
Erizoned <erizoned@gmail.com>
2023-11-11 00:55:47 +0600

    changed file app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const sum = <p>{5 + 2}</p>
+const Me = {
+  name: 'Антон',
+  lastname: 'Романов',
+  age: 17
+}
 
+const style = {
+  fontFamily: 'Sans',
+  color: 'red',
+  fontSize: '25px'
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>2 Задание</h1>
+          {sum}
+        <hr></hr>
+        <h1>3 задание</h1>
+      <p style={style}>{Me.name} {Me.lastname}, возраст: {Me.age} лет.</p>
     </div>
   );
 }
 
 export default App;
+
