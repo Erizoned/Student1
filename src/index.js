33632e25d08f712854b0ab92e4f8733e2648755d
Erizoned <erizoned@gmail.com>
2023-11-11 00:56:35 +0600

    changed file index

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './Calculator';
+import Sprav from './Sprav'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const calc = ReactDOM.createRoot(document.getElementById('Calculator'))
+const Spravi = ReactDOM.createRoot(document.getElementById('Sprav'))
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
+calc.render(
+    <Calculator />
+);
+Spravi.render(
+    <Sprav />
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
